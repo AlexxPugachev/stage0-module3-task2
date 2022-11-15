d1ad985f33ac12085e313b0fc15386c917f93652
AlexxPugachev <alexpugachevprod@gmail.com>
2022-11-15 12:59:02 +0200

    Done!

--- src/main/java/lang/print/gaps/task2/advancedNamingConvention.java
+++ src/main/java/lang/print/gaps/task2/advancedNamingConvention.java
@@ -3,14 +3,15 @@ package lang.print.gaps.task2;
 public class advancedNamingConvention {
     private static final int adult_age = 18;
 
-    private int AGE;
-    private int phoneNumber;
+    private int AGE = 25;
+    private int phoneNumber = 85447954;
+    final int friend13 = 13;
 
-    void CAllToFriend() {
-        //some code
+    void CAllToFriend(int friend) {
+        CAllToFriend(friend13);
     }
 
     void callByNumber(int Number) {
-        //some code
+        callByNumber(phoneNumber);
     }
 }
