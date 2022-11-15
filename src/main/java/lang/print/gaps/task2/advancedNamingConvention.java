86a62d86bedeff96f43101166b11a41d5b850cbb
AlexxPugachev <alexpugachevprod@gmail.com>
2022-11-15 13:59:48 +0200

    +

--- src/main/java/lang/print/gaps/task2/advancedNamingConvention.java
+++ src/main/java/lang/print/gaps/task2/advancedNamingConvention.java
@@ -4,16 +4,17 @@ public class advancedNamingConvention {
     private static final int adult_age = 18;
 
     private int AGE;
+    AGE = 18;
     private int phoneNumber;
+    phoneNumber = 66566656566;
 
-    void CAllToFriend() {
-        int friend = 23;
-        CAllToFriend(int friend);
-
+    void CAllToFriend(String[] args) {
+        int i;
+        i = 32;
+        CAllToFriend(int i);
     }
 
-    void callByNumber(int Number) {
-        int number = 5454545455;
-        callByNumber(int number);
+    void callByNumber(String[] args) {
+        callByNumber(phoneNumber);
     }
 }
